d4cd80a86d4b313ba6afbc9a0065fddb61bb253b
Sri Hari Karthick <sriharikarthikn@yahoo.in>
2019-01-06 08:02:23 +0530

    Added the standalone self requirement URL excel file to IP excel file converter, need to test with excel file maam gave

---
+++ xlWriteTemp.py
@@ -0,0 +1,21 @@
+import openpyxl
+from urlToApiIP import converter
+
+#For converting URL list of excel files to Api IP excel files
+
+def newWriter():
+    path = input("Enter the relative path to the file including the name: ")
+    wb = openpyxl.load_workbook(path)
+    wb2 = openpyxl.Workbook()
+    writeSheet = wb2.active
+    sheetNames = wb.get_sheet_names()
+    for i in sheetNames:
+        sheet = wb[i]
+        for j in range(2, sheet.max_row + 1):
+            user, ID = converter(sheet.cell(row = j, column = 2).value)
+            row = [user, ID]
+            writeSheet.append(row)
+    wb2.save("IP.xlsx")
+
+if __name__ == "__main__":
+    newWriter()
