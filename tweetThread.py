11712fdc24dad3f1908d18e7243b6d376b229e04
Sri Hari Karthick <sriharikarthikn@yahoo.in>
2019-02-12 07:53:23 +0530

    Added recursive call. Requests slows down and gives erraneous results on multiple concurrent requests

--- tweetThread.py
+++ tweetThread.py
@@ -5,29 +5,35 @@ from tweetCleaner import cleaner
 from bs4 import BeautifulSoup
 import requests
 import sys
+import time
 
 non_bmp_map = dict.fromkeys(range(0x10000, sys.maxunicode + 1), 0xfffd)
 tweetReplyContent = []
 
 def replyRet(url):
+	time.sleep(20)
 	response = requests.get(url)
 	soup = BeautifulSoup(response.content, "lxml")
 	tweetReplyList = soup.find(class_='stream')
-	tweetReply = tweetReplyList.find_all('p')
-	tweetReplyList = soup.findAll(attrs = {'class' : 'tweet-timestamp js-permalink js-nav js-tooltip'})
-	for content in tweetReply:
-		if content is not None:
-			content = content.prettify().translate(non_bmp_map)
-			tweetReplyContent.append(content)
-	user, tweetId = converter(url)
-	extraTweetReplyList, extraTweetReplyContent = extraTweets(user, tweetId)
-	for i in extraTweetReplyList:
-		tweetReplyList.append(i)
-	for i in extraTweetReplyContent:
-		tweetReplyContent.append(i)
-	for i in range(len(tweetReplyContent)):
-		tweetReplyContent[i] = cleaner(tweetReplyContent[i])
-	return tweetReplyList, tweetReplyContent
+	if tweetReplyList is not None:
+		tweetReply = tweetReplyList.find_all('p')
+		tweetReplyList = soup.findAll(attrs = {'class' : 'tweet-timestamp js-permalink js-nav js-tooltip'})
+		for content in tweetReply:
+			if content is not None:
+				content = content.prettify().translate(non_bmp_map)
+				tweetReplyContent.append(content)
+		user, tweetId = converter(url)
+		extraTweetReplyList, extraTweetReplyContent = extraTweets(user, tweetId)
+		if extraTweetReplyList is not None:
+			for i in extraTweetReplyList:
+				tweetReplyList.append(i)
+			for i in extraTweetReplyContent:
+				tweetReplyContent.append(i)
+		for i in range(len(tweetReplyContent)):
+			tweetReplyContent[i] = cleaner(tweetReplyContent[i])
+		return tweetReplyList, tweetReplyContent
+	else:
+		return None, None
 
 if __name__ == "__main__":
     #for testing whether it returns the values properly!
